ef8cf258483056fb51d9d1999db8987ac1676c82
Cristiano Alves da Silva Junior <cristianoalvesjr@gmail.com>
2018-09-04 13:55:38 -0300

    Added first pieces of code

---
+++ SoapMarket/js/home.js
@@ -0,0 +1,23 @@
+﻿function NewJqueryTask(taskTitle) {
+    taskName = $('<p class="task">' + taskTitle + '</p>');
+    taskName.click(function () {
+        $(this).css({
+            "text-decoration": "line-through"
+        });
+    });
+    return taskName;
+}
+
+$(document).ready(function () {
+    $("#button-add-task").click(function () {
+        taskTitle = $("input[name=task]").val();
+        taskName = NewJqueryTask(taskTitle);
+        taskColumn = $("<div/>", {
+            "class": "col"
+        });
+        taskColumn.append(taskName);
+        task = $('<div class="row"></div>');
+        task.append(taskColumn);
+        task.appendTo("#container-tasks");
+    });
+});
